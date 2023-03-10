4e4ae4fd22b5b7e1b1df61e76b1b70a9dc39ffba
fpascuales <fpascuales@gmail.com>
2023-03-10 07:27:25 +0100

    login solucionado

--- login.js
+++ login.js
@@ -14,22 +14,22 @@ const signUser = async (username, password) => {
         body: JSON.stringify(objectToSend)
     }
     await fetch("https://snake-back-three.vercel.app/players/registro", options);
-    // login(username, password)        //PENDIENTE REVISIÓN FUNCIÓN LOGIN
+    login(username, password)
 }
-// const login = async (username, password) => {
-//     const objectToSend = {
-//         user: username,
-//         password: password
-//     }
-//     const query = await fetch('https://snake-back-three.vercel.app/players/login', {
-//             method: 'POST',
-//             headers: {
-//                 'Consent-Type': 'application/json'
-//             },
-//             body: JSON.stringify(objectToSend)
-//         })
-//     const result = await query.json()
-//     localStorage.setItem("token", result.token)
-//     console.log(result.token)
-//     // canvaSnake()    
-// }
+const login = async (username, password) => {
+    const objectToSend = {
+        user: username,
+        password: password
+    }
+    const query = await fetch('https://snake-back-three.vercel.app/players/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(objectToSend)
+        })
+    const result = await query.json()
+    localStorage.setItem("token", result.token)
+    console.log(result.token)
+    // canvaSnake()    
+}
