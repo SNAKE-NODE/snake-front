8e9ea80cc2510790dabe4df84a961f92f2c17c31
adriandearias <deariasadrian@gmail.com>
2023-03-11 21:27:32 +0100

    added content to snake.js

--- snake.js
+++ snake.js
@@ -1,81 +1,169 @@
 //export
-function canvaSnake(){
-    const body$$ = document.body;
-    console.log(body$$);
-    const div$$ = document.createElement('div');
-    const spanScore$$ = document.createElement('span');
-    const canvas$$ = document.createElement('canvas');
-    const resetBtn$$ = document.createElement('button');
+function canvaSnake() {
+  const body$$ = document.body;
+  const div$$ = document.createElement("div");
+  const canvas$$ = document.createElement("canvas");
 
-    body$$.appendChild(div$$);
-    div$$.setAttribute('id', "gameContent");
-    div$$.appendChild(canvas$$);
-    div$$.appendChild(spanScore$$);
-    div$$.appendChild(resetBtn$$);
-    canvas$$.setAttribute('id', "gameTable");
-    canvas$$.setAttribute('width', "500");
-    canvas$$.setAttribute('height', "500");
-    spanScore$$.setAttribute('id', "score");
-    resetBtn$$.setAttribute('id', "resetBtn");
-    resetBtn$$.innerHTML = "Restart Game";
-    spanScore$$.innerHTML = 0;
-    
-    //canvas
-    const snakeBoard = document.querySelector("#gameTable");
-    const ctx = snakeBoard.getContext("2d");
-    const scoreTxt = document.querySelector("#score");
-    const gameWidth = snakeBoard.width;
-    const gameHeight = snakeBoard.height;
-    const boardBg = "black";
-    const snakeColor = "white";
-    const snakeBorder ="yellow";
-    const foodColor = "red";
-    const unitSize = 25;
-    let running = false;
-    let xVelocity = unitSize;
-    let yVelocity = 0;
-    let foodX;
-    let foodY;
-    let score  = 0;
-    //each object pos is a part of the body of the snake
-    let snake = [
-        {x:unitSize*4, y:0},
-        {x:unitSize*3, y:0},
-        {x:unitSize*2, y:0},
-        {x:unitSize, y:0},
-        {x:0, y:0}
-    ];
-    window.addEventListener("keydown", changeDirection);
-    resetBtn$$.addEventListener("click", resetGame);
+  body$$.appendChild(div$$);
+  div$$.setAttribute("id", "gameContent");
+  div$$.appendChild(canvas$$);
+  canvas$$.setAttribute("id", "gameTable");
+  canvas$$.setAttribute("width", "800");
+  canvas$$.setAttribute("height", "800");
 
-    gameStart();
-    createFood();
-    drawFood();
+  //canvas
+  const ctx = canvas$$.getContext("2d");
+  let tileCount = 20;
+  let tileSize = 18;
+  let headX = 10;
+  let headY = 10;
+  let xvelocity = 0;
+  let yvelocity = 0;
+  let appleX = 5;
+  let appleY = 5;
+  let score = 0;
+  // array for snake parts
+  const snakeParts = [];
+  let tailLength = 2; //initial parts of snake
 
-    function gameStart(){};
-    function nextTick(){};
-    function clearBoard(){};
+  document.body.addEventListener("keydown", keyDown);
 
-    function createFood(){
-        function randomFood (min, max){
-            const randomNumber = Math.round((Math.random()*(max-min)+min)/unitSize)*unitSize; //now all numbers are div*25
-            return randomNumber;
-        }
-        foodX = randomFood(0, gameWidth - unitSize);
-        console.log(foodX);
-    };
+  class snakePart {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
 
-    function drawFood(){
-        ctx.fillStyle = foodColor;
-        ctx.fillRect(foodX, foodY, unitSize, unitSize);
-    };
+  function drawGame() {
+    score = 0;
+    let speed = 7;
+    clearScreen();
+    drawSnake();
+    changeSnakePosition();
+    checkCollision();
+    drawApple();
+    drawScore();
+    setTimeout(drawGame, 1000 / speed); //update screen 7 times a second
+  }
 
-    function moveSnake(){};
-    function drawSnake(){};
-    function changeDirection(){};
-    function checkGameOver(){};
-    function displayGameOver(){};
-    function resetGame(){};
+  function drawSnake() {
+    ctx.fillStyle = "white";
+    for (let i = 0; i < snakeParts.length; i++) {
+      //draw snake parts
+      let part = snakeParts[i];
+      ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+    }
+    snakeParts.push(new snakePart(headX, headY));
+  }
+
+  function clearScreen() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas$$.clientWidth, canvas$$.clientHeight);
+  }
+
+  function keyDown() {
+    //up
+    if (event.keyCode == 38) {
+      if (yvelocity == 1) return; //prevent snake from moving in opposite direction
+      yvelocity = -1;
+      xvelocity = 0;
+    }
+    //down
+    if (event.keyCode == 40) {
+      if (yvelocity == -1) return; //prevent snake from moving in opposite direction
+      yvelocity = 1;
+      xvelocity = 0;
+    }
+
+    //left
+    if (event.keyCode == 37) {
+      if (xvelocity == 1) return; //prevent snake from moving in opposite direction
+      yvelocity = 0;
+      xvelocity = -1;
+    }
+    //right
+    if (event.keyCode == 48) {
+      if (xvelocity == -1) return; //prevent snake from moving in opposite direcction
+      yvelocity = 0;
+      xvelocity = 1;
+    }
+  }
+
+  function changeSnakePosition() {
+    headX = headX + xvelocity;
+    headY = headY + yvelocity;
+  }
+
+  function drawApple() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
+  }
+
+  function checkCollision() {
+    if (appleX == headX && appleY == headY) {
+      appleX = Math.floor(Math.random() * tileCount);
+      appleY = Math.floor(Math.random() * tileCount);
+    }
+  }
+
+  function checkCollision() {
+    if (appleX == headX && appleY == headY) {
+      appleX = Math.floor(Math.random() * tileCount);
+      appleY = Math.floor(Math.random() * tileCount);
+      tailLength++;
+    }
+  }
+  function drawScore() {
+    ctx.fillStyle = "white";
+    ctx.font = "20px verdena";
+    ctx.fillText("Score: " + score, canvas$$.clientWidth - 90, 30);
+  }
+
+  function isGameOver() {
+    let gameOver = false;
+    //check whether game has started
+    if (yvelocity === 0 && xvelocity === 0) {
+      return false;
+    }
+    if (headX < 0) {
+      //if snake hits left wall
+      gameOver = true;
+    } else if (headX === tileCount) {
+      //if snake hits right wall
+      gameOver = true;
+    } else if (headY < 0) {
+      //if snake hits wall at the top
+      gameOver = true;
+    } else if (headY === tileCount) {
+      //if snake hits wall at the bottom
+      gameOver = true;
+    }
+
+    //stop the game when snake bumps into itself
+
+    for (let i = 0; i < snakeParts.length; i++) {
+      let part = snakeParts[i];
+      if (part.x === headX && part.y === headY) {
+        //check whether any part of snake is occupying the same space
+        gameOver = true;
+        break; // to break out of for loop
+      }
+    }
+    //display text Game Over
+    if (gameOver) {
+      ctx.fillStyle = "white";
+      ctx.font = "50px verdana";
+      ctx.fillText(
+        "Game Over! ",
+        canvas.clientWidth / 6.5,
+        canvas.clientHeight / 2
+      ); //position our text in center
+    }
+
+    return gameOver; // this will stop the execution of the draw game method
+  }
+  drawGame();
 }
 
 canvaSnake();
